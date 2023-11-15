6908eaf082fd79deb264b2151ba04d09d5c2b49e
Ahmed-11222 <150014274+Ahmed-11222@users.noreply.github.com>
2023-11-15 10:59:13 +0400

    Add files via upload

---
+++ job acceptance.py
@@ -0,0 +1,10 @@
+x=input("Enter is your gender")
+if x=="M":
+    age=int (input(("enter your age")))
+    if (24<=age<=30):
+        print("Accepted")
+    else:
+        print("Not accepted")
+else:
+        print ("Not accepted")
+        
