7b1808969e7c74018591eb3aaaf18da74049f404
Ahmed-11222 <150014274+Ahmed-11222@users.noreply.github.com>
2023-11-16 14:45:16 +0400

    Add files via upload

---
+++ number.py
@@ -0,0 +1,21 @@
+n=19
+x=int (input( "enter a number "))
+
+while (x!=""):
+    if (x==n):
+        print("Win")
+        break
+
+    elif (x<n):
+        print("Go up")
+        continue
+    elif (x>n):
+        print("Go down")
+        continue
+         
+         
+         
+
+        
+        
+        
---
+++ whileLoop.py
@@ -0,0 +1,7 @@
+start=1
+n=ord(input(" "))
+sum_=0
+while (start<=n):
+    sum_+=start
+    start+=1
+print(sum_)
