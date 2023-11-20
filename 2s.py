7846de8032e8cacaf6fa82b40c50be89fe633fde
Ahmed-11222 <150014274+Ahmed-11222@users.noreply.github.com>
2023-11-20 14:50:14 +0400

    Add files via upload

---
+++ 2s.py
@@ -0,0 +1,11 @@
+n=int (input(" "))
+sum_=0
+for i in range (1, 1+n):
+    result="2"*i
+    sum_+=int(result)
+
+print(sum_)
+    
+
+ 
+
---
+++ calculategrade2.py
@@ -0,0 +1,19 @@
+answer='abaacb'
+
+a=input("write your answers ")
+
+grade=0
+
+for i in range (len(answer)):
+    if (answer[i]==a[i]):
+        grade+=1
+print(grade)
+
+
+
+
+        
+
+    
+   
+    
